709c012ee54e542f30322de2fbe34c2ea370bf0f
Szymon <szymonnie2891@gmail.com>
2025-02-21 14:04:50 +0100

    bug fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,20 +2,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'home',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'menu',
-    renderMode: RenderMode.Server
-  },
-  {
     path: 'game',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'ending',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   },
   {
     path: '',
